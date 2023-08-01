25013096ac8c6afb93161b9f5ad56be91a81ad79
dimanranawaka <dimanranawaka@gmail.com>
2023-08-01 13:52:32 +0530

    Simple_POS - Striplet

--- Simple_POS/src/servlet/CustomerServlet.java
+++ Simple_POS/src/servlet/CustomerServlet.java
@@ -17,7 +17,6 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
     }
 
     @Override
